96b24f083ca18b5e00491d5cf0a263a7be3e1349
Vishwanath Sangale <vishsangale@gmail.com>
2021-03-19 10:55:04 -0700

    Encode module with message passing skelton for graph matching network

--- graph-matching/model.py
+++ graph-matching/model.py
@@ -1,7 +1,43 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, MessagePassing
+
+
+class Encoder(MessagePassing):
+    def __init__(self):
+        super().__init__()
+        in_node_channels = 2
+        out_node_channels = 2
+        hidden_node_features = 16
+        self.node_mlp = MLP(in_node_channels, out_node_channels, hidden_node_features)
+
+        in_edge_channels = 2
+        out_edge_channels = 2
+        hidden_edge_channels = 16
+        self.edge_mlp = MLP(in_edge_channels, out_edge_channels, hidden_edge_channels)
+
+    def forward(self, data):
+        # TODO steps
+        # take mlp of nodes and do message passing from current layer to next
+
+        pass
+
+
+class MLP(nn.Module):
+    def __init__(
+        self, in_channels: int, out_channels: int, hidden_features: int
+    ) -> None:
+        self.layer1 = nn.Linear(in_channels, hidden_features)
+        self.layer2 = nn.Linear(hidden_features, out_channels)
+
+    def forward(self, data):
+        x = self.layer1(data)
+        x = F.relu(x)
+
+        x = self.layer2(x)
+        x = F.relu(x)
+        return x
 
 
 class MatchingGCN(nn.Module):
